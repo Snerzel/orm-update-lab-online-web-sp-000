0752e9b48bdf8242858620073cf0f98e863aa4c7
Cherissa Woudenberg <snerzel@comcast.net>
2021-06-30 19:43:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,16 +45,16 @@ class Student
       student_new
   end
 
-  def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
+    def self.new_from_db(row)
+      id = row[0]
+      name = row[1]
+      grade = row[2]
+      self.new(id, name, grade)
   end
 
-  def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
-    DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
+    def self.find_by_name(name)
+      sql = "SELECT * FROM students WHERE name = ?"
+      DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
   end
 
   def update
